cea13738913aeab8ca9c011ab3635eaa703e31ce
Vivek Singh <viveksingh9301@gmail.com>
2025-06-22 00:22:11 +0530

    Add godotenv package and load environment variables in main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Welcome to Lumina API.")
+
+	// Load variables from .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading the .env file")
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/viveksingh-01/lumina-api
 
 go 1.24.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
