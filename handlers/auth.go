e8e246bff05d99fbc38cc892b7580d0d4972b862
Vivek Singh <viveksingh9301@gmail.com>
2025-06-29 10:43:07 +0530

    Implement request validation and JSON decoding in Register handler

--- handlers/auth.go
+++ handlers/auth.go
@@ -1,5 +1,10 @@
 package handlers
 
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/viveksingh-01/lumina-api/models"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
@@ -10,5 +15,23 @@ func SetUserCollection(c *mongo.Collection) {
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
+	// Validate request method
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method, please use POST method.", http.StatusMethodNotAllowed)
+		return
+	}
+	// Validate request body to not be nil
+	if r.Body == nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	// Decoding the request body in JSON to struct format
+	var user models.User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, "Error occurred while decoding request JSON", http.StatusBadRequest)
+		return
+	}
+
 	// TODO
 }
