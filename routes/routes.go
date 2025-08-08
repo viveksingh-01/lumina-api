7e80ab8b3a51ecdc3991618d06cba9e7cfea59e9
Vivek Singh <viveksingh9301@gmail.com>
2025-08-08 18:12:43 +0530

    Secure private routes with auth-middleware

--- routes/routes.go
+++ routes/routes.go
@@ -3,10 +3,15 @@ package routes
 import (
 	"github.com/gorilla/mux"
 	"github.com/viveksingh-01/lumina-api/handlers"
+	"github.com/viveksingh-01/lumina-api/middlewares"
 )
 
 func RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/register", handlers.Register)
 	router.HandleFunc("/login", handlers.Login)
-	router.HandleFunc("/api/chat", handlers.HandleChat).Methods("POST")
+
+	secured := router.PathPrefix("/api").Subrouter()
+	// Use auth-middleware to secure paths with prefixed with '/api'
+	secured.Use(middlewares.AuthMiddleware)
+	secured.HandleFunc("/chat", handlers.HandleChat)
 }
