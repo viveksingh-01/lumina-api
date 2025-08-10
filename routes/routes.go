1b9f848e6f7f1f6e5a17d7b38fdffabb8e42df07
Vivek Singh <viveksingh9301@gmail.com>
2025-08-11 00:02:10 +0530

    Expose me-API handler with the secured route

--- routes/routes.go
+++ routes/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/viveksingh-01/lumina-api/handlers"
 	"github.com/viveksingh-01/lumina-api/middlewares"
@@ -13,5 +15,7 @@ func RegisterRoutes(router *mux.Router) {
 	secured := router.PathPrefix("/api").Subrouter()
 	// Use auth-middleware to secure paths with prefixed with '/api'
 	secured.Use(middlewares.AuthMiddleware)
+
+	secured.HandleFunc("/me", handlers.MeHandler).Methods(http.MethodGet)
 	secured.HandleFunc("/chat", handlers.HandleChat)
 }
